219e4fdaf3ddf5821b98cf7559d098e436cc6f8f
Noelani West <noelani.rose14@gmail.com>
2022-12-04 10:29:03 -0600

    set up pages with routes

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({});
+// export default {
+//     title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
+//   description: `A simple, clean, and modern theme a startup or businesses' marketing website.`,
+//   url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+//   name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+//   enableThemeSwitcher: true,
+// }
